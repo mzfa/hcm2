f4cc4a42e37b755ed9656a49795e9a8bee1db008
mzfa <zulfikarfebri1505@gmail.com>
2025-01-10 15:30:02 +0700

    BUG FIX -- PERBAIKAN BUG SAAT UPDATE PEGAWAI

--- app/Http/Controllers/StrukturController.php
+++ app/Http/Controllers/StrukturController.php
@@ -132,10 +132,10 @@ class StrukturController extends Controller
     {
         // $id = Crypt::decrypt($id);
         // dd($data);
-        $struktur = DB::table('struktur')->get();
+        $struktur = DB::table('struktur')->whereNull('deleted_at')->get();
         // dd($struktur);
         $text = "Data tidak ditemukan";
-        if($data = DB::table('struktur')->where(['struktur_id' => $id])->first()){
+        if($data = DB::table('struktur')->where(['struktur_id' => $id])->whereNull('deleted_at')->first()){
             return view('struktur.edit', compact('struktur','data'));
         }
         return $text;
@@ -157,63 +157,63 @@ class StrukturController extends Controller
         // $res = json_decode('{"active":true,"address":[{"city":"Jakarta","country":"ID","extension":[{"extension":[{"url":"province","valueCode":"31"},{"url":"city","valueCode":"3172"},{"url":"district","valueCode":"317204"},{"url":"village","valueCode":"3172041002"}],"url":"https://fhir.kemkes.go.id/r4/StructureDefinition/administrativeCode"}],"line":["Jl. Tipar Cakung No.46, RT.2/RW.1, Sukapura Kec. Cilincing, Jkt Utara, Daerah Khusus Ibukota Jakarta"],"postalCode":"14140","type":"both","use":"work"}],"id":"2c0eaa43-09ed-4516-a0b9-9f7c309c6744","identifier":[{"system":"http://sys-ids.kemkes.go.id/organization/28c7f233-4382-4fff-aeca-7f1e5174ba74","use":"official"}],"meta":{"lastUpdated":"2023-11-18T04:16:01.921750+00:00","versionId":"MTcwMDI4MDk2MTkyMTc1MDAwMA"},"name":"SEKERTARIS - RS UMUM PEKERJA","partOf":{"reference":"Organization/bb14e1fe-d768-471b-9c64-c5d12272bcec"},"resourceType":"Organization","telecom":[{"system":"phone","use":"work","value":"+6285777789022"},{"system":"email","use":"work","value":"rumah.sakit.pekerja@gmail.com"},{"system":"url","use":"work","value":"www.webrs.rsumumpekerja-kbn.com"}],"type":[{"coding":[{"code":"dept","display":"Hospital Department","system":"http://terminology.hl7.org/CodeSystem/organization-type"}]}]}');
 
         // dd($res->id);
-        if(isset($request->satusehat_id)){
-            $curl = curl_init();
+        // if(isset($request->satusehat_id)){
+        //     $curl = curl_init();
 
-            curl_setopt_array($curl, array(
-            CURLOPT_URL => env('BASE_URL_SATU_SEHAT').'api/organisasi/'.$satusehat_id,
-            CURLOPT_RETURNTRANSFER => true,
-            CURLOPT_ENCODING => '',
-            CURLOPT_MAXREDIRS => 10,
-            CURLOPT_TIMEOUT => 0,
-            CURLOPT_FOLLOWLOCATION => true,
-            CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
-            CURLOPT_CUSTOMREQUEST => 'PATCH',
-            CURLOPT_POSTFIELDS => 'nama_organisasi='.$request->nama_struktur.'&id='.$satusehat_id.'&referensi='.$referensi_id,
-            ));
+        //     curl_setopt_array($curl, array(
+        //     CURLOPT_URL => env('BASE_URL_SATU_SEHAT').'api/organisasi/'.$satusehat_id,
+        //     CURLOPT_RETURNTRANSFER => true,
+        //     CURLOPT_ENCODING => '',
+        //     CURLOPT_MAXREDIRS => 10,
+        //     CURLOPT_TIMEOUT => 0,
+        //     CURLOPT_FOLLOWLOCATION => true,
+        //     CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+        //     CURLOPT_CUSTOMREQUEST => 'PATCH',
+        //     CURLOPT_POSTFIELDS => 'nama_organisasi='.$request->nama_struktur.'&id='.$satusehat_id.'&referensi='.$referensi_id,
+        //     ));
 
-            $response = curl_exec($curl);
+        //     $response = curl_exec($curl);
 
-            curl_close($curl);
-            $res = json_decode($response);
-            $res = json_decode($res);
-            if(empty($res->id)){
-                dd($response);
-            }
-            // dd($res);
+        //     curl_close($curl);
+        //     $res = json_decode($response);
+        //     $res = json_decode($res);
+        //     if(empty($res->id)){
+        //         dd($response);
+        //     }
+        //     // dd($res);
 
-        }else{
-            $curl = curl_init();
-            curl_setopt_array($curl, array(
-            CURLOPT_URL => env('BASE_URL_SATU_SEHAT').'api/organisasi',
-            CURLOPT_RETURNTRANSFER => true,
-            CURLOPT_ENCODING => '',
-            CURLOPT_MAXREDIRS => 10,
-            CURLOPT_TIMEOUT => 0,
-            CURLOPT_FOLLOWLOCATION => true,
-            CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
-            CURLOPT_CUSTOMREQUEST => 'POST',
-            CURLOPT_POSTFIELDS => array('nama_organisasi' => $request->nama_struktur,'referensi' => $referensi_id),
-            ));
+        // }else{
+        //     $curl = curl_init();
+        //     curl_setopt_array($curl, array(
+        //     CURLOPT_URL => env('BASE_URL_SATU_SEHAT').'api/organisasi',
+        //     CURLOPT_RETURNTRANSFER => true,
+        //     CURLOPT_ENCODING => '',
+        //     CURLOPT_MAXREDIRS => 10,
+        //     CURLOPT_TIMEOUT => 0,
+        //     CURLOPT_FOLLOWLOCATION => true,
+        //     CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+        //     CURLOPT_CUSTOMREQUEST => 'POST',
+        //     CURLOPT_POSTFIELDS => array('nama_organisasi' => $request->nama_struktur,'referensi' => $referensi_id),
+        //     ));
     
-            $response = curl_exec($curl);
+        //     $response = curl_exec($curl);
     
-            curl_close($curl);
-            $res = json_decode($response);
-            $res = json_decode($res);
-            if(empty($res->id)){
-                dd($response);
-            }
-            $satusehat_id = $res->id;
-            // dd($res);
-        }
+        //     curl_close($curl);
+        //     $res = json_decode($response);
+        //     $res = json_decode($res);
+        //     if(empty($res->id)){
+        //         dd($response);
+        //     }
+        //     $satusehat_id = $res->id;
+        //     // dd($res);
+        // }
         // dd($satusehat_id);
 
         $data = [
             'nama_struktur' => $request->nama_struktur,
             'akronim' => $request->akronim,
             'parent_id' => $request->parent_id,
-            'satusehat_id' => $satusehat_id,
+            // 'satusehat_id' => $satusehat_id,
             'updated_by' => Auth::user()->id,
             'updated_at' => now(),
         ];
